abf38766d95cbc31ddfa07a985c710670c109499
Yihan Gao <gaoyihan@gmail.com>
2015-07-13 22:46:51 -0500

    decompressor implemented

--- decompression.cpp
+++ decompression.cpp
@@ -20,14 +20,43 @@ void Decompressor::Init() {
         std::unique_ptr<Model> model(GetModelFromDescription(&byte_reader_, schema_, i));
         model_.push_back(std::move(model));
     }
+    implicit_prefix_ = 0;
+    ReadTuplePrefix();
+}
+
+void Decompressor::ReadTuplePrefix() {
+    while (implicit_prefix_ < ((unsigned)1 << implicit_length_)) {
+        bool bit = byte_reader_.ReadBit();
+        if (bit) 
+            ++ implicit_prefix_;
+        else
+            break;
+    }
 }
 
 void Decompressor::ReadNextTuple(Tuple* tuple) {
-    // Todo:
+    unsigned int implicit_prefix_count_ = 1;
+    ProbInterval PIt(0, 1), PIb(0, 1);
+    for (size_t i = 0; i < schema_.attr_type.size(); ++i) {
+        ProbDist* prob_dist = model_[attr_order_[i]]->GetProbDist(*tuple, PIt, PIb);
+        while (!prob_dist->IsEnd()) {
+            bool bit;
+            if (implicit_prefix_count_ <= implicit_length_) {
+                bit = ((implicit_prefix_ >> (implicit_length_ - implicit_prefix_count_)) & 1); 
+                ++ implicit_prefix_count_;
+            } else
+                bit = byte_reader_.ReadBit();
+            prob_dist->FeedBit(bit);
+        }
+        PIt = prob_dist->GetPIt();
+        PIb = prob_dist->GetPIb();
+        tuple->attr[attr_order_[i]].reset(prob_dist->GetResult());
+    }
+    ReadTuplePrefix();
 }
 
 bool Decompressor::HasNext() const {
-    // Todo:
+    return (implicit_prefix_ == ((unsigned)1 << implicit_length_)); 
 }
 
 }  // namespace db_compress
